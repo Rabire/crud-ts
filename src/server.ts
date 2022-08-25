fe829ea58bf8b02d4629299241971f61663efef0
Rabire <rabireh@outlook.fr>
2022-08-25 11:34:36 +0200

    express setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Coucou");
+import express, { Request, Response } from "express";
+
+const PORT = 3040;
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  return res.send("hello world");
+});
+
+app.listen(PORT, () => console.log("server is running on port " + PORT));
